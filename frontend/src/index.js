a2a8e347e16613f78ab8d0f1ca886e2d5c9890fd
RomanRiyanov <yezgo123@gmail.com>
2022-10-16 21:55:08 +0600

    [main] ПР15 исправления после первого ревью

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
